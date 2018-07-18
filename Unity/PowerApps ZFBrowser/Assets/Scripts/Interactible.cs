7fc415534d7124718438304ffd119f66439562dc
Sam Oh <t-saoh@microsoft.com>
2018-07-18 11:02:25 -0700

    Added all the files for my current project. Currently a WIP, will be working on it today and tomorrow. Feel free to pull and look around/mess around locally, but try not to push any new code (unless we talk about it) as I envision merge conflicts with Unity could be a pain. Currently am trying to figure out interaction with the browser in Unity. Test it out to see if you can successfully see the browser as well.

---
+++ Unity/PowerApps ZFBrowser/Assets/Scripts/Interactible.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public abstract class InteractibleAction : MonoBehaviour
+{
+    public abstract void PerformAction();
+}
+
+public class Interactible : MonoBehaviour, IInputClickHandler {
+
+    public ZenFulcrum.EmbeddedBrowser.Browser browser;
+
+    [SerializeField]
+    private InteractibleAction interactibleAction;
+
+    // Use this for initialization
+    private void Start () {
+
+        // if interactible doesn't contain a boxcollider, then add one
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+	}
+
+    void IInputClickHandler.OnInputClicked(InputClickedEventData eventData)
+    {
+        browser.forceClick();
+        // Just performing a tagalong action
+        if (interactibleAction != null)
+        {
+            interactibleAction.PerformAction();
+        }
+    }
+}
